4ed90d2ac7a876e05b2da59512a05a8b0c2365e5
DrTrogalstaid <trogtrogalstaid@gmail.com>
2026-08-08 23:17:54 -0400

    Added sequence and pathway items - These will be items a character can hold that effects what's on their character sheet

--- module/data/_module.mjs
+++ module/data/_module.mjs
@@ -8,3 +8,6 @@ export {default as LordOfTheMysteriesItemBase} from "./item/base-item.mjs";
 export {default as LordOfTheMysteriesItem} from "./item/item-item.mjs";
 export {default as LordOfTheMysteriesFeature} from "./item/item-feature.mjs";
 export {default as LordOfTheMysteriesSpell} from "./item/item-spell.mjs";
+// Class Items
+export {default as LordOfTheMysteriesPathway} from "./item/class/item-pathway.mjs";
+export {default as LordOfTheMysteriesSequence} from "./item/class/item-sequence.mjs";
