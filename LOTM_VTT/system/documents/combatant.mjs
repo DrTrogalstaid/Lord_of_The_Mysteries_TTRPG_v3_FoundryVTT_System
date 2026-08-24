44d18274bc99f238ae9d3845bbd0783294501469
Clayton <111898737+Rasaec5@users.noreply.github.com>
2026-08-23 19:05:57 -0600

    Delete LOTM_VTT directory

--- LOTM_VTT/system/documents/combatant.mjs
+++
@@ -1,5 +0,0 @@
-export default class LOTM_VTTCombatant extends Combatant {
-    _getInitiativeFormula() {
-        return String(CONFIG.Combat.initiative.formula || game.system.initiative || this.actor.getInitiativeFormula());
-    }
-}
--- LOTM_VTT/system/hooks/render-chat-log.mjs
+++
@@ -1,5 +0,0 @@
-import LOTM_VTTChatCard from "../documents/chat-card.mjs";
-
-export function renderChatLog(app, html, data) {
-    LOTM_VTTChatCard.activateListeners(html);
-}
